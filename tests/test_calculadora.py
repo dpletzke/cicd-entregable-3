0e2fb59924c3a81abb3c4aa8ae77c7494a6659b4
dpletzke <dpletzke@gmail.com>
2026-04-19 14:14:50 -0500

    put functions in html

--- tests/test_calculadora.py
+++ tests/test_calculadora.py
@@ -2,20 +2,24 @@
 import pytest
 from app.calculadora import sumar, restar, multiplicar, dividir, potencia, modulo
 
+
 def test_sumar():
     assert sumar(2, 3) == 5
     assert sumar(-1, 1) == 0
     assert sumar(0, 0) == 0
+
 
 def test_restar():
     assert restar(5, 2) == 3
     assert restar(1, -1) == 2
     assert restar(0, 0) == 0
 
+
 def test_multiplicar():
     assert multiplicar(2, 3) == 6
     assert multiplicar(-1, 5) == -5
     assert multiplicar(0, 10) == 0
+
 
 def test_dividir():
     assert dividir(10, 2) == 5.0
@@ -23,10 +27,12 @@ def test_dividir():
     with pytest.raises(ZeroDivisionError):
         dividir(1, 0)
 
+
 def test_potencia():
     assert potencia(2, 3) == 8
     assert potencia(5, 0) == 1
     assert potencia(4, -1) == 0.25
+
 
 def test_modulo():
     assert modulo(10, 3) == 1
