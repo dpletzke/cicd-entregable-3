cf91f3052467ef22c395a02f366f10a7f190bef4
dpletzke <dpletzke@gmail.com>
2026-04-19 11:47:39 -0500

    rerun black

--- app/app.py
+++ app/app.py
@@ -34,9 +34,11 @@ def index():
 
     return render_template("index.html", resultado=resultado)
 
+
 @app.route("/health")
 def health():
     return "OK", 200
+
 
 if __name__ == "__main__":
     app.run(debug=False, port=5000, host="0.0.0.0")
